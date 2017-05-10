afd57daa1f941fb52596b4d4bee21b607bb2d895
Aline Bender <abender@outlook.com.br>
2017-05-10 18:50:40 -0300

    Texto inserido

--- app/screens/cadastro/CadastroCliente.js
+++ app/screens/cadastro/CadastroCliente.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import { AppRegistry, Text, StyleSheet, TouchableOpacity, View } from 'react-native';
 import Modal from 'react-native-modal';
 import NavigationBar from 'react-native-navbar';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import { Sae } from 'react-native-textinput-effects';
 
 export default class ModalTester extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nomeText: "Cícero",
+      dataNascimentoText: '19/05/1990',
+      emailText: 'cicinho@mail.com',
+      perfilText: 'Perfil Cliente',
+      tagsText: '#Arroz #Feijão #eBatata'
+    };
+  }
+
   state = {
     isModalVisible: false
   }
@@ -14,11 +27,30 @@ export default class ModalTester extends Component {
 
   render () {
     return (
-      <View style={styles.container}>
+      <View style={{flex:1}}>
         <NavigationBar
           title={titleConfig}
           tintColor="#95c9db"
         />
+        <View style={styles.container}>
+        <Text style={styles.baseText}>
+        <Text style={styles.titleText} onPress={this.onPressTitle}>
+          {this.state.nomeText}{'\n'}{'\n'}{'\n'}
+        </Text>
+        <Text numberOfLines={5}>
+          {this.state.dataNascimentoText}{'\n'}{'\n'}
+        </Text>
+        <Text numberOfLines={5}>
+          {this.state.emailText}{'\n'}{'\n'}
+        </Text>
+        <Text numberOfLines={5}>
+          {this.state.perfilText}{'\n'}{'\n'}
+        </Text>
+        <Text numberOfLines={5}>
+          {this.state.tagsText}{'\n'}{'\n'}
+        </Text>
+      </Text>
+        </View>
         <TouchableOpacity onPress={this._showModal}>
           <Text>Tela onde ficara edicao</Text>
         </TouchableOpacity>
@@ -32,6 +64,7 @@ export default class ModalTester extends Component {
   }
 }
 
+
   //CSS
   const titleConfig = {
     title: 'Cadastro Cliente',
@@ -41,6 +74,17 @@ export default class ModalTester extends Component {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  baseText: {
+    fontFamily: 'Cochin',
+
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
