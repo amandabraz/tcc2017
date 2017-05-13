8dcf767f30cce508d672fc3a0a28e9525acdea89
larissaespinosa <larissasitta.espinosa@gmail.com>
2017-05-12 22:34:02 -0300

    Background alterado, imagem para perfil e textinput effects

--- app/screens/cadastro/index.js
+++ app/screens/cadastro/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Button, TextInput, ScrollView, Alert} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Button, TextInput, ScrollView, Alert, Image} from 'react-native';
 import NavigationBar from 'react-native-navbar';
 import DatePicker from 'react-native-datepicker';
-
+import MaterialsIcon from 'react-native-vector-icons/MaterialIcons';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import { Kohana } from 'react-native-textinput-effects';
+import ImagePicker from 'react-native-image-picker';
 
 const onButtonPress = () => { Alert.alert('Bem vindo Vendedor'); };
 const onButtonPressComprar = () => { Alert.alert('Bem vindo '); };
+
 
 class cadastro extends Component {
   constructor(props) {
@@ -13,27 +17,40 @@ class cadastro extends Component {
 
    this.state = {
     date: '',
+    nome: '',
+    email: '',
+    senha:'',
   };
  }
-
-  render() {
+ render() {
     return (
-      <View style= {{flex: 3, backgroundColor: '#ffffff'}}>
+      <View style= {{flex: 3}}>
+      <Image source={require('./img/cake1.jpg')}
+      style={styles.backgroundImage}>
       <NavigationBar
        title={titleConfig}
        tintColor="#f5f5f5"/>
         <ScrollView>
-   <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.setState({text})}
-          placeholder="Nome Completo"
-          placeholderTextColor = "#ffb6c1"
-        />
 
-        <DatePicker
-        style={{width: 378,
-        height: 48}}
+   <View style={styles.container}>
+   <Text>{'\n'}</Text>
+
+   <Image style={{width: 200, height: 200, borderRadius: 100}}
+          source={require('./img/cameraa.jpg')}/>
+
+   <Kohana
+     style={{ backgroundColor: 'transparent' }}
+     label={'Nome Completo'}
+     iconClass={FontAwesomeIcon}
+     onChangeText={(nome) => this.setState({nome: nome})}
+    iconName={'user'}
+     iconColor={'#f5f5f5'}
+     labelStyle={{ color: '#f5f5f5', fontSize: 20, fontFamily: 'Roboto', textAlign: 'center' }}
+     inputStyle={{ color: '#f5f5f5', fontSize: 20, fontFamily: 'Roboto', textAlign: 'center' }}
+   />
+
+      <DatePicker
+        style={{width: 378, height: 48}}
         date={this.state.date}
         mode="date"
         placeholder="Data de Nascimento"
@@ -48,43 +65,54 @@ class cadastro extends Component {
            marginLeft: 0
          },
          placeholderText: {
-           color: '#ffb6c1',
+           color: '#f5f5f5',
            fontFamily: 'Roboto',
            fontSize: 20
          },
          dateText: {
-           color: '#f08080',
+           color: '#f5f5f5',
            fontFamily: 'Roboto',
            fontSize: 20
          }
        }}
         onDateChange={(date) => {this.setState({date: date});}}
         />
-
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.setState({text})}
-          placeholder="Email"
-          placeholderTextColor = "#ffb6c1"
+        <Kohana
+          style={{ backgroundColor: 'transparent' }}
+          label={'Email'}
+          iconClass={FontAwesomeIcon}
+          onChangeText={(email) => this.setState({email: email})}
+          iconName={'envelope'}
+          iconColor={'#f5f5f5'}
+          labelStyle={{ color: '#f5f5f5', fontSize: 20, fontFamily: 'Roboto', textAlign: 'center' }}
+          inputStyle={{ color: '#f5f5f5', fontSize: 20, fontFamily: 'Roboto', textAlign: 'center' }}
         />
 
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.setState({text})}
-          placeholder="Senha"
-          placeholderTextColor = "#ffb6c1"
+        <Kohana
+          style={{ backgroundColor: 'transparent' }}
+          label={'Senha'}
+          iconClass={FontAwesomeIcon}
+          iconName={'lock'}
+          onChangeText={(senha) => this.setState({senha: senha})}
+          iconColor={'#f5f5f5'}
+          labelStyle={{ color: '#f5f5f5', fontSize: 20, fontFamily: 'Roboto', textAlign: 'center' }}
+          inputStyle={{ color: '#f5f5f5', fontSize: 20, fontFamily: 'Roboto', textAlign: 'center' }}
           secureTextEntry={true}
         />
 
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.setState({text})}
-          placeholder="Confirmação da senha"
-          placeholderTextColor = "#ffb6c1"
+        <Kohana
+          style={{ backgroundColor: 'transparent', borderColor: '#778899', borderWidth: 0.15 }}
+          label={'Confirmação de Senha'}
+          iconClass={FontAwesomeIcon}
+          iconName={'lock'}
+          iconColor={'#f5f5f5'}
+          labelStyle={{ color: '#f5f5f5', fontSize: 20, fontFamily: 'Roboto', textAlign: 'center' }}
+          inputStyle={{ color: '#f5f5f5', fontSize: 20, fontFamily: 'Roboto', textAlign: 'center' }}
           secureTextEntry={true}
         />
 
         </View>
+        <Text>{'\n'}{'\n'}{'\n'}</Text>
 
          <View style={{flexDirection: 'row', justifyContent: 'space-between', margin: 5}}>
          <Button
@@ -98,6 +126,7 @@ class cadastro extends Component {
           onPress={onButtonPressComprar}/>
      </View>
      </ScrollView>
+     </Image>
        </View>
     );
   }
@@ -113,14 +142,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 3,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
+    alignItems: 'center'
   },
+
   texto: {
     color: '#f08080',
     fontSize: 30,
     fontFamily: 'Roboto',
     textAlign: 'center'
+  },
+
+  backgroundImage: {
+      flex: 1,
+      width: null,
+      height: null,
   },
 
     input:{
