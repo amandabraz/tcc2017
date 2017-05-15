681adc1fe4679a54fe68adf3a6a7bc47383fc0b4
larissaespinosa <larissasitta.espinosa@gmail.com>
2017-05-15 12:16:11 -0300

    Acesso a galeria

--- android/app/src/main/java/com/tcc2017/MainApplication.java
+++ android/app/src/main/java/com/tcc2017/MainApplication.java
@@ -3,6 +3,7 @@ package com.tcc2017;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PickerPackage(),
             new ImagePickerPackage(),
             new VectorIconsPackage()
       );
