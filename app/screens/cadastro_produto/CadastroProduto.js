89b6a7e77c1c6093f12ff6949415b44715849730
larissaespinosa <larissasitta.espinosa@gmail.com>
2017-05-24 10:40:07 -0300

    inseri o dropdown, mas ainda não funciona

--- app/screens/cadastro_produto/CadastroProduto.js
+++ app/screens/cadastro_produto/CadastroProduto.js
@@ -19,9 +19,14 @@ import DatePicker from 'react-native-datepicker';
 import CameraRollPicker from 'react-native-camera-roll-picker';
 import Camera from 'react-native-camera';
 import ImagePicker from 'react-native-image-crop-picker';
+import TagInput from 'react-native-tag-input';
 
-
-
+import DropDown, {
+  Select,
+  Option,
+  OptionList,
+  updatePosition
+} from 'react-native-dropdown';
 
 //dimensão da janela
 //const { width, height } = Dimensions.get("window");
@@ -34,8 +39,54 @@ export default class CadastroProduto extends Component {
 
   this.state = {
    date: '',
+   tags: [],
+   ingredientes: [],
+   quantidade: '',
+   categoria: '',
  };
 }
+
+
+componentDidMount() {
+    updatePosition(this.refs['SELECT1']);
+    updatePosition(this.refs['SELECT2']);
+
+    updatePosition(this.refs['OPTIONLIST']);
+  }
+
+  _getOptionList() {
+    return this.refs['OPTIONLIST'];
+}
+
+  _quantidade(numero) {
+
+  this.setState({
+      ...this.state,
+      quantidade: numero
+    });
+  }
+
+    _categoria(cat) {
+
+    this.setState({
+        ...this.state,
+        categoria: cat
+      });
+    }
+
+
+onChangeTags = (tags) => {
+    this.setState({
+      tags,
+    });
+};
+
+onChangeIngredientes = (ingredientes) => {
+    this.setState({
+      ingredientes,
+    });
+};
+
 pickMultiple(){
   ImagePicker.openPicker({
   multiple: true
@@ -51,20 +102,41 @@ abrircamera(){
 });
 }
   render() {
+    const inputProps = {
+      placeholder: 'Ingredientes',
+      placeholderTextColor: '#dc143c',
+      fontSize: 20,
+      fontFamily: 'Roboto',
+      textAlign: 'center',
+      height:300
+};
+
+const inputProp = {
+  placeholder: 'Tags',
+  placeholderTextColor: '#dc143c',
+  fontSize: 20,
+  fontFamily: 'Roboto',
+  textAlign: 'center',
+  height:300
+};
+
     return (
-      <View style= {{flex: 3, backgroundColor: '#ffffff'}}>
+      <View style= {{flex: 1, backgroundColor: '#ffffff'}}>
       <NavigationBar
        title={titleConfig}
        tintColor="#f5f5f5"/>
         <ScrollView>
 
-   <View style={styles.container}>
+      <View style={styles.container}>
+
         <TextInput
           style={styles.input}
           onChangeText={(text) => this.setState({text})}
           placeholder="Nome"
           placeholderTextColor = "#ffb6c1"
         />
+
+
 
         <TextInput
           style={styles.input}
@@ -103,29 +175,101 @@ abrircamera(){
         onDateChange={(date) => {this.setState({date: date});}}
         />
 
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.setState({text})}
-          placeholder="Categoria"
-          placeholderTextColor = "#ffb6c1"
-        />
+      <View style={{ width: 378, height: 86, alignItems: 'center'}}>
+      <Text>      </Text>
+        <TagInput
+          value={this.state.ingredientes}
+          onChange={this.onChangeIngredientes}
+          tagColor="pink"
+          tagTextColor="white"
+          tagAlign="center"
+          inputProps={inputProps}
+          numberOfLines={15}/>
 
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.setState({text})}
-          placeholder="Ingredientes"
-          placeholderTextColor = "#ffb6c1"
-          secureTextEntry={true}
-        />
+        </View>
 
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.setState({text})}
-          placeholder="Quantidade"
-          placeholderTextColor = "#ffb6c1"
-          secureTextEntry={true}
-        />
+        <Select
+          width={378}
+          ref="SELECT1"
+          ptionListRef={this._getOptionList.bind(this)}
+          styleText={{color: '#f08080',
+                              fontFamily: 'Roboto',
+                              fontSize: 20,
+                              textAlign: 'center'}}
+          defaultValue="Selecione a categoria"
+          onSelect={this._categoria.bind(this)}>
+          <Option value = {{id : "Doce"}}
+                  styleText={{color: '#f08080',
+                              fontFamily: 'Roboto',
+                              fontSize: 20,
+                              textAlign: 'center'}}>Doce</Option>
+          <Option styleText={{color: '#f08080',
+                              fontFamily: 'Roboto',
+                              fontSize: 20,
+                              textAlign: 'center'}}>Salgado</Option>
+          <Option styleText={{color: '#f08080',
+                              fontFamily: 'Roboto',
+                              fontSize: 20,
+                              textAlign: 'center'}}>Bebida</Option>
 
+          </Select>
+
+
+        <Select
+            width={378}
+            ref="SELECT2"
+            optionListRef={this._getOptionList.bind(this)}
+            styleText={{color: '#f08080',
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                textAlign: 'center'}}
+            defaultValue="Selecione a quantidade"
+            onSelect={this._quantidade.bind(this)}>
+            <Option value = {{id : "1"}}
+                    styleText={{color: '#f08080',
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                textAlign: 'center'}}>1</Option>
+            <Option styleText={{color: '#f08080',
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                textAlign: 'center'}}>2</Option>
+            <Option styleText={{color: '#f08080',
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                textAlign: 'center'}}>3</Option>
+            <Option styleText={{color: '#f08080',
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                textAlign: 'center'}}>4</Option>
+            <Option styleText={{color: '#f08080',
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                textAlign: 'center'}}>5</Option>
+            <Option styleText={{color: '#f08080',
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                textAlign: 'center'}}>6</Option>
+            <Option styleText={{color: '#f08080',
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                textAlign: 'center'}}>7</Option>
+            <Option styleText={{color: '#f08080',
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                textAlign: 'center'}}>8</Option>
+            <Option styleText={{color: '#f08080',
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                textAlign: 'center'}}>9</Option>
+            <Option styleText={{color: '#f08080',
+                                fontFamily: 'Roboto',
+                                fontSize: 20,
+                                textAlign: 'center'}}>10</Option>
+          </Select>
+          <OptionList ref="OPTIONLIST"/>
+
+        <Text>      </Text>
         <Text style={styles.foto}>Foto do Produto</Text>
         <View style={{flexDirection: 'row', justifyContent: 'space-between', margin: 5}}>
         <Button
@@ -142,22 +286,23 @@ abrircamera(){
 
         </View>
 
-      //  <CameraRollPicker
-        //  callback={this.getSelectedImages} />
-
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.setState({text})}
-          placeholder="Tags"
-          placeholderTextColor = "#ffb6c1"
-        />
-
-
+        <Text>      </Text>
+        <View style={{ width: 378, height: 86, alignItems: 'center'}}>
+        <TagInput
+          value={this.state.tags}
+          onChange={this.onChangeTags}
+          tagColor="pink"
+          tagTextColor="white"
+          tagAlign="center"
+          inputProps={inputProp}
+          numberOfLines={15}/>
         </View>
         <Button
          title ="Salvar"
          color="#ffa07a"
          onPress={onButtonPress}/>
+
+         </View>
      </ScrollView>
        </View>
     );
@@ -177,6 +322,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#ffffff',
   },
+
   texto: {
     color: '#f08080',
     fontSize: 30,
@@ -202,5 +348,20 @@ const styles = StyleSheet.create({
       fontSize: 20,
       textAlign: 'center',
     },
+
+    tag: {
+    justifyContent: 'center',
+    backgroundColor: '#e0e0e0',
+    borderRadius: 16,
+    paddingLeft: 12,
+    paddingRight: 12,
+    height: 32,
+    margin: 4,
+  },
+
+  tagLabel: {
+    fontSize: 13,
+    color: 'rgba(0, 0, 0, 0.87)',
+},
 
 });
