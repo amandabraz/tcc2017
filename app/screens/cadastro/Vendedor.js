e66807992946204b70dabe611c9c067ee7af5041
Amandabraz <amandabbraz@gmail.com>
2017-06-01 00:36:54 -0300

    Estilo atualizado para a tela + Tags foram importadas da tela Cliente

--- app/screens/cadastro/Vendedor.js
+++ app/screens/cadastro/Vendedor.js
@@ -3,12 +3,14 @@
 * CPF, Palavras Chave de identificação do produto, Nome fantasia da Empresa, e Meios de Pagamentos aceitos na compra
 */
 import React, { Component } from 'react';
-import { TextInput, ScrollView, StyleSheet, View, Alert, ToastAndroid } from 'react-native';
-import { Tile, List, ListItem, Button } from 'react-native-elements';
+import { TextInput, ScrollView, StyleSheet, View, Alert, ToastAndroid, Text } from 'react-native';
+import { Tile, Button } from 'react-native-elements';
 import CheckBox from 'react-native-check-box';
 import { Kohana } from 'react-native-textinput-effects';
 import MaterialsIcon from 'react-native-vector-icons/MaterialIcons';
 import NavigationBar from 'react-native-navbar';
+import TagInput from 'react-native-tag-input';
+
 
 class Vendedor extends Component {
   constructor(props) {
@@ -17,17 +19,10 @@ class Vendedor extends Component {
         diasArray: ["Dinheiro", "Cartão de crédito", "Transferência", "Paypal"],
         cpf: '',
         nomeLoja: '',
-        meiosPagamento: []
+        meiosPagamento: [],
+        tags: []
     }
   }
-
-// Config navbar
-  const titleConfig = {
-    title: 'Cadastro do Vendedor',
-    tintColor: "#dc143c",
-    fontFamily: 'Roboto',
-  };
-
 
   handleFinalizarPress = () => {
     const {
@@ -40,8 +35,10 @@ class Vendedor extends Component {
       "usuario": 1,
       "nomeFantasia": nomeLoja,
       "cpf": cpf,
-      "meiosPagamento": meiosPagamento
+      "meiosPagamento": meiosPagamento,
+      "tags": tags
     }
+    //Alert.alert("Vendedor", JSON.stringify(vendedor));
     // TODO: restante dos parametros. alterar url abaixo para o servidor (enfiar essa constante em algum buraco)
      fetch('http://10.0.2.2:8080/vendedor', {
         method: 'POST',
@@ -106,65 +103,82 @@ class Vendedor extends Component {
   }
 
   render() {
+    const inputProps = {
+      keyboardType: 'default',
+      placeholder: 'Tags para identificar seus produtos',
+      placeholderTextColor: '#402B2E'
+    };
+
     return (
-      <NavigationBar
-        title={titleConfig}
-        tintColor="#95c9db"
-      />
-      <ScrollView>
-        <List>
-          <Kohana
-            style={{ height: 45 }}
-            label={'CPF'}
-            iconClass={MaterialsIcon}
-            iconName={'account-box'}
-            iconColor={'#658091'}
-            labelStyle={{ color: '#402B2E', fontSize: 20, fontFamily: 'Roboto' }}
-            inputStyle={{ color: '#B27A81', fontSize: 20, fontFamily: 'Roboto' }}
-            onChangeText={(cpf) => this.setState({cpf})}
-            value={this.state.cpf}
-            keyboardType="phone-pad"
-            returnKeyType="next"
+      <View style={{flex: 1}}>
+          <NavigationBar
+            title={titleConfig}
+            tintColor="aquamarine"
           />
-          <Kohana
-            style={{ height: 45 }}
-            label={'Nome da sua loja'}
-            iconClass={MaterialsIcon}
-            iconName={'store'}
-            iconColor={'#658091'}
-            labelStyle={{ color: '#402B2E', fontSize: 20, fontFamily: 'Roboto' }}
-            inputStyle={{ color: '#B27A81', fontSize: 20, fontFamily: 'Roboto' }}
-            onChangeText={(nomeLoja) => this.setState({nomeLoja})}
-            value={this.state.nomeLoja}
-            returnKeyType="next"
-          />
-          <ListItem
-            title="Meios de pagamento aceitos"
-            hideChevron
-          />
-          <View style={styles.container}>
-              <ScrollView>
-                  {this.renderView()}
-              </ScrollView>
-          </View>
-          <ListItem
-            title="Tags para seus produtos"
-            hideChevron
-          />
-          <TextInput
-            style={styles.singleLine}
-            returnKeyType="next"
-          />
-        </List>
-        <Button
-          title="Finalizar"
-          buttonStyle={{ marginBottom: 20 }}
-          onPress={this.handleFinalizarPress}
-        />
-      </ScrollView>
+        <View style={styles.container}>
+          <ScrollView style={{ backgroundColor: '#fff' }}>
+              <Kohana
+                style={{ height: 45 }}
+                label={'CPF'}
+                iconClass={MaterialsIcon}
+                iconName={'account-box'}
+                iconColor={'#658091'}
+                labelStyle={{ color: '#402B2E', fontSize: 20, fontFamily: 'Roboto' }}
+                inputStyle={{ color: '#B27A81', fontSize: 20, fontFamily: 'Roboto' }}
+                onChangeText={(cpf) => this.setState({cpf})}
+                value={this.state.cpf}
+                keyboardType="phone-pad"
+                returnKeyType="next"
+              />
+              <Kohana
+                style={{ height: 45 }}
+                label={'Nome da sua loja'}
+                iconClass={MaterialsIcon}
+                iconName={'store'}
+                iconColor={'#658091'}
+                labelStyle={{ color: '#402B2E', fontSize: 20, fontFamily: 'Roboto' }}
+                inputStyle={{ color: '#B27A81', fontSize: 20, fontFamily: 'Roboto' }}
+                onChangeText={(nomeLoja) => this.setState({nomeLoja})}
+                value={this.state.nomeLoja}
+                returnKeyType="next"
+              />
+              <Text style={{ padding: 16, color: '#402B2E', fontSize: 20, fontFamily: 'Roboto', fontWeight: 'bold' }}>
+                  Meios de pagamento aceitos
+              </Text>
+              <View style={styles.container}>
+                  <ScrollView>
+                      {this.renderView()}
+                  </ScrollView>
+              </View>
+
+              <View style={{ flexDirection: 'column', flex: 1, height: 150 }}>
+                <TagInput
+                  value={this.state.tags}
+                  onChange={(tags) => this.setState({tags,})}
+                  tagColor="aquamarine"
+                  tagTextColor="darkblue"
+                  inputProps={inputProps}
+                  numberOfLines={15}
+                />
+              </View>
+            <Button
+              title="Finalizar"
+              buttonStyle={{ padding: 16, marginBottom: 3 }}
+              onPress={this.handleFinalizarPress}
+            />
+          </ScrollView>
+        </View>
+      </View>
     );
   }
 }
+
+const titleConfig = {
+  title: 'Cadastro do Vendedor',
+  tintColor: "darkblue",
+  fontFamily: 'Roboto',
+};
+
 var styles = StyleSheet.create({
   multiline: {
     height: 60,
@@ -172,26 +186,16 @@ var styles = StyleSheet.create({
     padding: 4,
     marginBottom: 10,
   },
-  eventLabel: {
-    margin: 3,
-    fontSize: 12,
-  },
   singleLine: {
     fontSize: 16,
     padding: 4,
     height: 45,
   },
-  singleLineWithHeightTextInput: {
-    height: 30,
-  },
-  hashtag: {
-    color: 'blue',
-    fontWeight: 'bold',
-  },
   container: {
       flex: 1,
-      backgroundColor: '#f3f2f2',
-      marginTop:30
+      justifyContent: 'space-between',
+      backgroundColor: '#fff',
+      padding: 15
   },
   item: {
       flexDirection: 'row',
