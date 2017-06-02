77f361b6abaf1127fd4071106eabc211aa4caddc
Amandabraz <amandabbraz@gmail.com>
2017-06-02 19:21:57 -0300

    Reorganizacao codigo do router.js e criação de mocks de telas futuras

--- app/navigation/router.js
+++ app/navigation/router.js
@@ -4,11 +4,7 @@ import { Icon } from 'react-native-elements';
 import MaterialsIcon from 'react-native-vector-icons/MaterialIcons';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
-
-//Coisas comentadas vieram do exemplo que utilizei,
-//retirado deste link: https://hackernoon.com/getting-started-with-react-navigation-the-navigation-solution-for-react-native-ea3f4bd786a4
-// Não retirei tudo pois achei que pudesse ser útil...
-
+// TELAS CRIADAS (TODAS DEVEM ESTAR LISTADAS)
 import Login from '../screens/login/Login';
 import Cadastro from '../screens/cadastro/Cadastro';
 import Vendedor from '../screens/cadastro/Vendedor';
@@ -17,152 +13,143 @@ import PerfilCliente from '../screens/perfil/PerfilCliente';
 import PerfilVendedor from '../screens/perfilVendedor/PerfilVendedor';
 import HomeVendedor from '../screens/home/HomeVendedor';
 import HomeCliente from '../screens/home/HomeCliente';
+import ProdutosFavoritos from '../screens/produto/ProdutosFavoritos';
+import RankingProdutos from '../screens/produto/RankingProdutos';
+import GerenciaProduto from '../screens/produto/GerenciaProduto';
+import BuscaProduto from '../screens/produto/BuscaProduto';
+import ConfiguracaoCliente from '../screens/configuracao/ConfiguracaoCliente';
+import ConfiguracaoVendedor from '../screens/configuracao/ConfiguracaoVendedor';
+import Estatisticas from '../screens/estatisticas/Estatisticas';
 
-
-// export const FeedStack = StackNavigator({
-//   Feed: {
-//     screen: Feed,
-//     navigationOptions: {
-//       title: 'Feed',
-//     },
-//   },
-//   Details: {
-//     screen: UserDetail,
-//     navigationOptions: {
-//       title: ({ state }) => `${state.params.name.first.toUpperCase()} ${state.params.name.last.toUpperCase()}`
-//     },
-//   },
-// });
 
 /**
 MENU SOMENTE PARA CLIENTE
 **/
 export const TabsCliente = TabNavigator({
-  Favoritos: {
-    screen: HomeCliente,
-    navigationOptions: {
-      tabBar: {
-        icon: ({ tintColor }) => <Icon name="star" size={25} color={tintColor} />
+    Favoritos: {
+      screen: ProdutosFavoritos,
+      navigationOptions: {
+        tabBar: {
+          icon: ({ tintColor }) => <Icon name="star" size={25} color={tintColor} />
+        },
       },
     },
-  },
-  Ranking: {
-    screen: HomeCliente,
-    navigationOptions: {
-      tabBar: {
-        icon: ({ tintColor }) => <FontAwesomeIcon name="bar-chart" size={20} color={tintColor} />
+    Ranking: {
+      screen: RankingProdutos,
+      navigationOptions: {
+        tabBar: {
+          icon: ({ tintColor }) => <FontAwesomeIcon name="bar-chart" size={20} color={tintColor} />
+        },
       },
     },
-  },
-  Home: {
-    screen: HomeCliente,
-    navigationOptions: {
-      tabBar: {
-        icon: ({ tintColor }) => <Icon name="home" size={30} color={tintColor} />
+    Home: {
+      screen: HomeCliente,
+      navigationOptions: {
+        tabBar: {
+          icon: ({ tintColor }) => <Icon name="home" size={30} color={tintColor} />
+        },
       },
     },
-  },
-  Perfil: {
-    screen: PerfilCliente,
-    navigationOptions: {
-      tabBar: {
-        icon: ({ tintColor }) => <Icon name="account-box" size={25} color={tintColor} />
+    Perfil: {
+      screen: PerfilCliente,
+      navigationOptions: {
+        tabBar: {
+          icon: ({ tintColor }) => <Icon name="account-box" size={25} color={tintColor} />
+        },
       },
     },
-  },
-  Configuração: {
-    screen: HomeCliente,
-    navigationOptions: {
-      tabBar: {
-        icon: ({ tintColor }) => <Icon name="settings" size={25} color={tintColor} />
+    Configuração: {
+      screen: ConfiguracaoCliente,
+      navigationOptions: {
+        tabBar: {
+          icon: ({ tintColor }) => <Icon name="settings" size={25} color={tintColor} />
+        },
       },
-    },
-  }
-},{
-  tabBarComponent: TabView.TabBarBottom,
-  tabBarPosition: 'bottom',
-  swipeEnabled: false,
-  animationEnabled: false,
-  backBehavior: 'none',
-  tabBarOptions: {
-    showLabel: false,
-    activeTintColor: '#8fbc8f',
-    inactiveTintColor: '#fff',
-    labelStyle: {
-      fontSize: 12,
-    },
-    style: {
-      backgroundColor: '#2f4f4f',
-    },
-  }
-});
+    }
+  },{
+    tabBarComponent: TabView.TabBarBottom,
+    tabBarPosition: 'bottom',
+    swipeEnabled: false,
+    animationEnabled: false,
+    backBehavior: 'none',
+    tabBarOptions: {
+      showLabel: false,
+      activeTintColor: '#8fbc8f',
+      inactiveTintColor: '#fff',
+      labelStyle: {
+        fontSize: 12,
+      },
+      style: {
+        backgroundColor: '#2f4f4f',
+      },
+    }
+  });
 
 /**
 MENU SOMENTE PARA VENDEDORES
 **/
 export const TabsVendedor = TabNavigator({
-  Loja: {
-    screen: HomeVendedor,
-    navigationOptions: {
-      tabBar: {
-        icon: ({ tintColor }) => <Icon name="store" size={25} color={tintColor} />
+    Loja: {
+      screen: GerenciaProduto,
+      navigationOptions: {
+        tabBar: {
+          icon: ({ tintColor }) => <Icon name="store" size={25} color={tintColor} />
+        },
       },
     },
-  },
-  Estatísticas: {
-    screen: HomeVendedor,
-    navigationOptions: {
-      tabBar: {
-        icon: ({ tintColor }) => <FontAwesomeIcon name="bar-chart" size={20} color={tintColor} />
+    Estatísticas: {
+      screen: HomeVendedor,
+      navigationOptions: {
+        tabBar: {
+          icon: ({ tintColor }) => <FontAwesomeIcon name="bar-chart" size={20} color={tintColor} />
+        },
       },
     },
-  },
-  Home: {
-    screen: HomeVendedor,
-    navigationOptions: {
-      tabBar: {
-        icon: ({ tintColor }) => <Icon name="home" size={30} color={tintColor} />
+    Home: {
+      screen: HomeVendedor,
+      navigationOptions: {
+        tabBar: {
+          icon: ({ tintColor }) => <Icon name="home" size={30} color={tintColor} />
+        },
       },
     },
-  },
-  Perfil: {
-    screen: PerfilVendedor,
-    navigationOptions: {
-      tabBar: {
-        icon: ({ tintColor }) => <Icon name="account-box" size={25} color={tintColor} />
+    Perfil: {
+      screen: PerfilVendedor,
+      navigationOptions: {
+        tabBar: {
+          icon: ({ tintColor }) => <Icon name="account-box" size={25} color={tintColor} />
+        },
       },
     },
-  },
-  Configuração: {
-    screen: HomeVendedor,
-    navigationOptions: {
-      tabBar: {
-        icon: ({ tintColor }) => <Icon name="settings" size={25} color={tintColor} />
+    Configuração: {
+      screen: ConfiguracaoVendedor,
+      navigationOptions: {
+        tabBar: {
+          icon: ({ tintColor }) => <Icon name="settings" size={25} color={tintColor} />
+        },
       },
-    },
-  }
-},{
-  tabBarComponent: TabView.TabBarBottom,
-  tabBarPosition: 'bottom',
-  swipeEnabled: false,
-  animationEnabled: false,
-  backBehavior: 'none',
-  tabBarOptions: {
-    showLabel: false,
-    activeTintColor: '#8fbc8f',
-    inactiveTintColor: '#fff',
-    labelStyle: {
-      fontSize: 12,
-    },
-    style: {
-      backgroundColor: '#4682b4',
-    },
-  }
+    }
+  },{
+    tabBarComponent: TabView.TabBarBottom,
+    tabBarPosition: 'bottom',
+    swipeEnabled: false,
+    animationEnabled: false,
+    backBehavior: 'none',
+    tabBarOptions: {
+      showLabel: false,
+      activeTintColor: '#8fbc8f',
+      inactiveTintColor: '#fff',
+      labelStyle: {
+        fontSize: 12,
+      },
+      style: {
+        backgroundColor: '#4682b4',
+      },
+    }
 });
 
 
 // TODO: Creio que aqui colocamos uma logica pra dizer: se usuario está logado, carregar dashboard, senão, carregar tela de login
- // TODO: verificar se manteremos tudo aqui ou não...
 export const Root = StackNavigator({
   Login: {
     screen: Login,
