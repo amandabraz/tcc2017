cb61f2f2bc668884e8b9eb0fdb324f08e245e630
Maiara Rodrigues <maiaraolirod@yahoo.com.br>
2017-06-05 04:41:46 -0300

    Finalizada primeira versão da tela de busca de produto

--- app/navigation/router.js
+++ app/navigation/router.js
@@ -6,8 +6,6 @@ import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 // TELAS CRIADAS (TODAS DEVEM ESTAR LISTADAS)
 import Login from '../screens/login/Login';
-import Cadastro from '../screens/cadastro/Cadastro';
-import Vendedor from '../screens/cadastro/Vendedor';
 import Cliente from '../screens/cadastro/Cliente';
 import PerfilCliente from '../screens/perfil/PerfilCliente';
 import PerfilVendedor from '../screens/perfil/PerfilVendedor';
@@ -20,12 +18,22 @@ import BuscaProduto from '../screens/produto/BuscaProduto';
 import ConfiguracaoCliente from '../screens/configuracao/ConfiguracaoCliente';
 import ConfiguracaoVendedor from '../screens/configuracao/ConfiguracaoVendedor';
 import Estatisticas from '../screens/estatisticas/Estatisticas';
+import Cadastro from '../screens/cadastro/Cadastro';
+import Vendedor from '../screens/cadastro/Vendedor';
 
 
 /**
 MENU SOMENTE PARA CLIENTE
 **/
 export const TabsCliente = TabNavigator({
+    Busca: {
+      screen: BuscaProduto,
+      navigationOptions: {
+        tabBar: {
+          icon: ({ tintColor }) => <Icon name="search" size={25} color={tintColor} />
+        },
+      },
+    },
     Favoritos: {
       screen: ProdutosFavoritos,
       navigationOptions: {
