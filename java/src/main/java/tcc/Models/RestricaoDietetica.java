0d156c271da8c070c23f24fd5974d9d2ad041da2
Aline Bender <abender@outlook.com.br>
2017-08-12 12:57:56 -0300

    [113][267] Adicionando underscore entre nome da tabela

--- java/src/main/java/tcc/Models/RestricaoDietetica.java
+++ java/src/main/java/tcc/Models/RestricaoDietetica.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name= "RESTRICAODIETETICA")
+@Table(name= "RESTRICAO_DIETETICA")
 public class RestricaoDietetica {
 
     @Id
