cb831d83223a91ec28ff9df2b26c2d3da7e680e6
Amandabraz <amandabbraz@gmail.com>
2017-08-21 22:52:23 -0300

    [120][378] Implementando métodos pra buscar e salvar ingredientes -- ainda não testado

--- java/src/main/java/tcc/Controllers/IngredienteController.java
+++ java/src/main/java/tcc/Controllers/IngredienteController.java
@@ -4,13 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import tcc.DAOs.IngredienteDAO;
 
-/**
- * Created by larissa on 24/05/17.
- */
-
 @RestController
 public class IngredienteController {
 
     @Autowired
     private IngredienteDAO IngredienteDao;
+
+    
 }
