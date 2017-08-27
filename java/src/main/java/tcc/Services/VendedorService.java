247a506d074e7e011d3eecdb67f71ec8e1947cf3
Amandabraz <amandabbraz@gmail.com>
2017-08-27 18:32:22 -0300

    [120][366] Altera login para ter id do Cliente e do Vendedor no state nas telas seguintes

--- java/src/main/java/tcc/Services/VendedorService.java
+++ java/src/main/java/tcc/Services/VendedorService.java
@@ -1,7 +1,23 @@
 package tcc.Services;
 
-/**
- * Created by amanda on 27/08/2017.
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tcc.DAOs.VendedorDAO;
+import tcc.Models.Usuario;
+import tcc.Models.Vendedor;
+
+@Service
 public class VendedorService {
+
+    @Autowired
+    private VendedorDAO vendedorDAO;
+
+    public Vendedor buscaVendedorPorUsuario(Usuario usuario) {
+        try {
+            return vendedorDAO.findByUsuario(usuario);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
 }
