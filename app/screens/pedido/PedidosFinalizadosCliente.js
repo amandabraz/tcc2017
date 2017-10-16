73b82e71a2eafc56c9ddbfcd22e9af9eb968c13f
abenderd <abender@outlook.com.br>
2017-10-16 18:20:13 -0200

    [493][116] Inserção de pedidos recusados e cancelados

--- app/screens/pedido/PedidosFinalizadosCliente.js
+++ app/screens/pedido/PedidosFinalizadosCliente.js
@@ -32,6 +32,8 @@ class PedidosFinalizadosCliente extends Component {
       userId: this.props.navigation.state.params.userId,
       clienteId: this.props.navigation.state.params.clienteId,
       pedidosFinalizados: [],
+      pedidosRecusados: [],
+      pedidosCancelados: [],
     };
     this.buscaDadosPedidosCliente();
   };
@@ -44,6 +46,20 @@ class PedidosFinalizadosCliente extends Component {
               this.setState({pedidosFinalizados: responseJson});
         }
       });
+      fetch(constante.ENDPOINT+'pedido/cliente/' + this.state.clienteId + '/status/' + 'Recusado')
+      .then((response) => response.json())
+        .then((responseJson) => {
+            if (!responseJson.errorMessage) {
+                this.setState({pedidosRecusados: responseJson});
+          }
+        });
+        fetch(constante.ENDPOINT+'pedido/cliente/' + this.state.clienteId + '/status/' + 'Cancelado')
+        .then((response) => response.json())
+          .then((responseJson) => {
+              if (!responseJson.errorMessage) {
+                  this.setState({pedidosCancelados: responseJson});
+            }
+          });
   };
 
 
@@ -106,6 +122,125 @@ pedidoFinalizado(){
       return views;
   }
 
+  pedidoRecusado(){
+    var views = [];
+    if(this.state.pedidosRecusados.length > 0){
+      for (i in this.state.pedidosRecusados){
+        let pedidoR = this.state.pedidosRecusados[i];
+        var dataNormal = new Date(pedidoR.dataFinalizacao);
+        var dataFinalizacao = dataNormal.getDate() + "/" + (dataNormal.getMonth() + 1) + "/" + dataNormal.getFullYear();
+        pedidoR.dataFinalizacao = dataFinalizacao;
+        views.push(
+          <View key={i} style={styles.oneResult1}>
+            <Accordion header={
+              <View style={{flexDirection: 'row'}}>
+              <View style = {{ width: '25%'}}>
+               <Image source={{uri: pedidoR.produto.imagemPrincipal}}
+                       style={styles.imagemPrincipal}/>
+              </View>
+            <View style={{width: '60%', alignSelf:'center'}}>
+             <Text style={styles.totalFont}> {pedidoR.produto.nome}</Text>
+             <Text style={{fontSize: 18}}> {pedidoR.dataFinalizacao}</Text>
+            </View>
+            <View style={{width: '5%',justifyContent: 'center'}}>
+            <Icon name="chevron-down" size={16} color={'lightgray'} type='font-awesome'/>
+            </View>
+        </View>
+            } content={
+              <View style={{paddingTop: 15}}>
+              <View style={{flexDirection: 'row', backgroundColor: 'rgba(0, 124, 138, 0.13)', borderRadius: 10, padding: 10, margin: 10}}>
+              <View style = {{ width: '20%'}}>
+                <Image source={{uri: pedidoR.produto.vendedor.usuario.imagemPerfil}}
+                       style={styles.imagemCliente}/>
+              </View>
+            <View style={{width: '80%'}}>
+              <Text style={styles.totalFont}> {pedidoR.produto.vendedor.usuario.nome}</Text>
+              <Text style={styles.oneResultfont}>Quantidade solicitada:
+              <Text style={styles.totalFont}> {pedidoR.quantidade}{'\n'}</Text>
+              </Text>
+              <Text style={styles.oneResultfont}>Valor do pedido:
+              <Text style={styles.totalFont}> R$ {pedidoR.valorCompra}{'\n'}</Text>
+              </Text>
+            </View>
+            </View>
+            </View>
+            }
+            underlayColor="white"
+            easing="easeOutCubic"/>
+          </View>
+        )}
+     } else {
+          views.push(
+            <View key={0} style={{alignItems: 'center'}}>
+            <Text style={{marginTop: 8, fontSize: 18, justifyContent: 'center', color: 'darkslategrey'}}>
+              Nenhum pedido recusado.
+            </Text>
+            </View>
+          )
+        }
+        return views;
+    }
+
+    pedidoCancelado(){
+      var views = [];
+      if(this.state.pedidosCancelados.length > 0){
+        for (i in this.state.pedidosCancelados){
+          let pedidoC = this.state.pedidosCancelados[i];
+          var dataNormal = new Date(pedidoC.dataFinalizacao);
+          var dataFinalizacao = dataNormal.getDate() + "/" + (dataNormal.getMonth() + 1) + "/" + dataNormal.getFullYear();
+          pedidoC.dataFinalizacao = dataFinalizacao;
+          views.push(
+            <View key={i} style={styles.oneResult1}>
+              <Accordion header={
+                <View style={{flexDirection: 'row'}}>
+                <View style = {{ width: '25%'}}>
+                 <Image source={{uri: pedidoC.produto.imagemPrincipal}}
+                         style={styles.imagemPrincipal}/>
+                </View>
+              <View style={{width: '60%', alignSelf:'center'}}>
+               <Text style={styles.totalFont}> {pedidoC.produto.nome}</Text>
+               <Text style={{fontSize: 18}}> {pedidoC.dataFinalizacao}</Text>
+              </View>
+              <View style={{width: '5%',justifyContent: 'center'}}>
+              <Icon name="chevron-down" size={16} color={'lightgray'} type='font-awesome'/>
+              </View>
+          </View>
+              } content={
+                <View style={{paddingTop: 15}}>
+                <View style={{flexDirection: 'row', backgroundColor: 'rgba(0, 124, 138, 0.13)', borderRadius: 10, padding: 10, margin: 10}}>
+                <View style = {{ width: '20%'}}>
+                  <Image source={{uri: pedidoC.produto.vendedor.usuario.imagemPerfil}}
+                         style={styles.imagemCliente}/>
+                </View>
+              <View style={{width: '80%'}}>
+                <Text style={styles.totalFont}> {pedidoC.produto.vendedor.usuario.nome}</Text>
+                <Text style={styles.oneResultfont}>Quantidade solicitada:
+                <Text style={styles.totalFont}> {pedidoC.quantidade}{'\n'}</Text>
+                </Text>
+                <Text style={styles.oneResultfont}>Valor do pedido:
+                <Text style={styles.totalFont}> R$ {pedidoC.valorCompra}{'\n'}</Text>
+                </Text>
+              </View>
+              </View>
+              </View>
+              }
+              underlayColor="white"
+              easing="easeOutCubic"/>
+            </View>
+          )}
+       } else {
+            views.push(
+              <View key={0} style={{alignItems: 'center'}}>
+              <Text style={{marginTop: 8, fontSize: 18, justifyContent: 'center', color: 'darkslategrey'}}>
+                Nenhum pedido recusado.
+              </Text>
+              </View>
+            )
+          }
+          return views;
+      }
+
+
   render() {
     return(
       <View style={styles.container}>
@@ -116,6 +251,18 @@ pedidoFinalizado(){
         </Text>
         </View>
         {this.pedidoFinalizado()}
+        <View style = {{margin: 5}}>
+        <Text style={{marginTop: 8, fontSize: 18, justifyContent: 'center', color: '#67A13F', fontWeight: 'bold'}}>
+          Pedidos Recusados
+        </Text>
+        </View>
+        {this.pedidoRecusado()}
+        <View style = {{margin: 5}}>
+        <Text style={{marginTop: 8, fontSize: 18, justifyContent: 'center', color: '#67A13F', fontWeight: 'bold'}}>
+          Pedidos Cancelados
+        </Text>
+        </View>
+        {this.pedidoCancelado()}
       </ScrollView>
       <Popup ref={popup => this.popup = popup }/>
     </View>
