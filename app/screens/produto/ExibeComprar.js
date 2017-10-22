67bd1bec117aef1068b2da8d379b29d6ed883d63
larissaespinosa <larissasitta.espinosa@gmail.com>
2017-10-22 19:14:38 -0200

    [140][506] Adicionada estrelinhas na tela de finalizar compra

--- app/screens/produto/ExibeComprar.js
+++ app/screens/produto/ExibeComprar.js
@@ -18,7 +18,8 @@ import Popup from 'react-native-popup';
 import HeaderImageScrollView, { TriggeringView } from 'react-native-image-header-scroll-view';
 import * as constante from '../../constantes';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
-import {RadioGroup, RadioButton} from 'react-native-flexi-radio-button'
+import {RadioGroup, RadioButton} from 'react-native-flexi-radio-button';
+import Rating from 'react-native-rating';
 
 const { width, height } = Dimensions.get("window");
 
@@ -173,6 +174,10 @@ export default class ExibeComprar extends Component {
     }
 
     render() {
+      const images = {
+      starFilled: require('./img/star_filled.png'),
+      starUnfilled: require('./img/star_unfilled.png')
+    }
         return(
             <View style={styles.container}>
               <HeaderImageScrollView
@@ -200,6 +205,19 @@ export default class ExibeComprar extends Component {
                 <Text style={styles.produtoText}>
                   {this.state.produto.nome}
                 </Text>
+                <Rating
+                  onChange={rating => console.log(rating)}
+                  initial= {2}
+                  editable={false}
+                  selectedStar={images.starFilled}
+                  unselectedStar={images.starUnfilled}
+                  stagger={80}
+                  maxScale={1.4}
+                  starStyle={{
+                    width: 30,
+                    height: 30
+                  }}
+                  />
                 <Text style={styles.quantidadeText}>
                   {this.state.quantidadeDis}
                 </Text>
