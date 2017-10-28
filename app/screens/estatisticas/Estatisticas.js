4fb4fd5616a452ee1263cd3957f078731465cb83
abenderd <abender@outlook.com.br>
2017-10-28 13:44:06 -0200

    [146][514] GRAFICOS FUCKING FUNCIONANDO

--- app/screens/estatisticas/Estatisticas.js
+++ app/screens/estatisticas/Estatisticas.js
@@ -6,13 +6,39 @@ import {
   TextInput,
   View
 } from 'react-native';
+import Chart from 'react-native-chart';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    chart: {
+        width: 200,
+        height: 200,
+    },
+});
+
+const data = [
+    [0, 1],
+    [1, 3],
+    [3, 7],
+    [4, 9],
+];
 
 class Estatisticas extends Component {
 
   render() {
     return(
-      <View>
-        <Text>Mock - Estatisticas</Text>
+      <View style={styles.container}>
+        <Chart
+          style={styles.chart}
+          data={data}
+          verticalGridStep={5}
+          type="line"
+        />
       </View>
     );
   }
