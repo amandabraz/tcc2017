066da42c44da01a29bd94209d010669c1d23ce55
Amandabraz <amandabbraz@gmail.com>
2017-10-28 20:00:20 -0200

    [497][455] inicio implementação onesignal

--- android/app/src/main/java/com/tcc2017/MainApplication.java
+++ android/app/src/main/java/com/tcc2017/MainApplication.java
@@ -3,6 +3,7 @@ package com.tcc2017;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.reactlibrary.RNAlertsPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new RNAlertsPackage(),
             new PickerPackage(),
             new RCTCameraPackage(),
