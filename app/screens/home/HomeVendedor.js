242bd0e6c63c84e900b81cfaa0fc1e94d2772b6f
larissaespinosa <larissasitta.espinosa@gmail.com>
2017-11-14 22:04:17 -0200

    adicionado o Loading

--- app/screens/home/HomeVendedor.js
+++ app/screens/home/HomeVendedor.js
@@ -22,6 +22,8 @@ import Popup from 'react-native-popup';
 import NavigationBar from 'react-native-navbar';
 import Switch from 'react-native-customisable-switch';
 import * as constante from '../../constantes';
+import Spinner from 'react-native-loading-spinner-overlay';
+
 
 const { width, height } = Dimensions.get("window");
 
@@ -63,7 +65,8 @@ class HomeVendedor extends Component {
         produtoVendido: 'Produto vendido',
         filtroMensal: true,
         alturaPedido: '1%',
-        alturaResumo: '100%'
+        alturaResumo: '100%',
+        carregou: true
     };
     this.buscaDadosPedido();
     this.buscaInformacoes();
@@ -126,6 +129,7 @@ class HomeVendedor extends Component {
               this.setState({clientesMantidos: clientec});
             }
           this.setState({refreshing:false});
+          this.setState({carregou: false});
       }});
   }
 
@@ -150,6 +154,7 @@ class HomeVendedor extends Component {
           this.setState({valorRecebido: 0});
           this.setState({clienteConquistados: 0});
           this.setState({ticketMedio: 0});
+          this.setState({carregou:false});
           this.buscaDadosPedido();
           this.pedidoSolicitado();
           this.buscaInformacoes();
@@ -279,6 +284,7 @@ render() {
                 <View style={{height: this.state.alturaPedido}}>
                   {this.pedidoSolicitado()}
                 </View>
+                <Spinner visible={this.state.carregou}/>
                 <View style={{height:this.state.alturaResumo}}>
                 <View style={styles.oneResultResumo}>
                 <View style={{alignItems: 'flex-end'}}>
