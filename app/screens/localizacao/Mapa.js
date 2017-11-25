75a82e934124eed496821fbc59353a19584051d3
Maiara Rodrigues <maiara.rodrigues@sensedia.com>
2017-11-25 14:38:42 -0200

    [594]Colocado link para mapa

---
+++ app/screens/localizacao/Mapa.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Mapa extends Component {
+  render() {
+    return (
+      <Text>Mapa ficará aqui</Text>
+    );
+  }
+}
