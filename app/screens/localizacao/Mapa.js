4490851a35d2562cda2ede3f48cc5023f0660f9e
maiarar <maiarrod@gmail.com>
2017-11-25 16:15:01 -0200

    [594] Implementado mapa simples

--- app/screens/localizacao/Mapa.js
+++ app/screens/localizacao/Mapa.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  View
+} from 'react-native';
+import MapView from 'react-native-maps'
+
+const { width, height } = Dimensions.get("window");
 
 class Mapa extends Component {
+
   render() {
     return (
-      <Text>Mapa ficará aqui</Text>
-    );
+      <View style={styles.container}>
+        <MapView style = {styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}/>
+      </View>
+    )
   }
+
 }
 
-Map.defaultProps = { ...Map };
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+})
+
+Mapa.defaultProps = { ...Mapa };
 
 export default Mapa;
