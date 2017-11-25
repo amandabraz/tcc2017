ae0aeea109f0d66df8489fbd4baa702173be0745
maiarar <maiarrod@gmail.com>
2017-11-25 16:43:16 -0200

    [594] Colocada barrinha superior de navegação

--- app/screens/localizacao/Mapa.js
+++ app/screens/localizacao/Mapa.js
@@ -1,33 +1,67 @@
 import React, { Component } from 'react';
 import {
   Dimensions,
+  ScrollView,
   StyleSheet,
+  TouchableOpacity,
+  RefreshControl,
   View
-} from 'react-native';
+} from 'react-native'
 import MapView from 'react-native-maps'
+import MaterialsIcon from 'react-native-vector-icons/MaterialIcons'
+import NavigationBar from 'react-native-navbar'
 
-const { width, height } = Dimensions.get("window");
+const { width, height } = Dimensions.get('window');
 
 class Mapa extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: false,   
+      carregou: true
+    }
+  };
+
   render() {
+    const {goBack} = this.props.navigation
+    const titleConfig = {
+      title: 'Mapa de Localização',
+      tintColor: '#4A4A4A',
+      fontFamily: 'Roboto',
+    }
     return (
-      <View style={styles.container}>
-        <MapView style = {styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}/>
+      <View style={{flex: 1}}>
+        <NavigationBar
+          leftButton={
+            <TouchableOpacity onPress={() => goBack()}>
+              <MaterialsIcon name='chevron-left' size={40} color={'#4A4A4A'}  style={{ padding: 3 }} />
+            </TouchableOpacity>
+          }
+          title={titleConfig}
+          style={{backgroundColor:''}}
+        />
+        <View style={{height: '95%'}}>
+          {this.loadMap()}
+        </View>
       </View>
     )
   }
 
+  loadMap() {
+    return <View style={styles.containerMapa}>
+      <MapView style={styles.mapa} initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }} />
+    </View>;
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerMapa: {
     position: 'absolute',
     top: 0,
     left: 0,
@@ -36,7 +70,7 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
-  map: {
+  mapa: {
     position: 'absolute',
     top: 0,
     left: 0,
